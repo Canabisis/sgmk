380a8149bdf89e343e0e0aa21d694cd6bac85d9c
Ar4ibald <canabisisx@gmail.com>
2019-11-30 13:33:45 +0700

    Complete

--- cluster/worker.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const pid = process.pid;
-http
-    .createServer((req, res) => {
-        res.end(`Hello`);
-    })
-    .listen(8800, () => {
-        console.log(`Worker started. Pid: ${pid}`);
-    });
